f29ea75b44a4b83c04e8cc243a788e5d2bbaa4d2
StefanWL <stefan_whittaker-lee@alumni.brown.edu>
2022-04-19 16:06:49 -0400

    Added certifications link to console dash

--- resources/views/console/dashboard.blade.php
+++ resources/views/console/dashboard.blade.php
@@ -8,6 +8,7 @@
         <li><a href="/console/projects/list">Manage Projects</a></li>
         <li><a href="/console/links/list">Manage Links</a></li>
         <li><a href="/console/types/list">Manage Types</a></li>
+        <li><a href="/console/certifications/list">Manage Certifications</a></li>
         <li><a href="/console/users/list">Manage Users</a></li>
         <li><a href="/console/logout">Log Out</a></li>
     </ul>
